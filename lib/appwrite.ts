9669ae710f61c7935d2205ed066297aac63b859d
Dean Blessed <deanb6538@gmail.com>
2026-04-24 20:05:32 +0100

    Message

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -177,7 +177,7 @@ export async function completePasswordRecovery(
 
 export async function googleSignIn() {
   try {
-    const redirectUrl = `${window.location.origin}/auth/callback`;
+    const redirectUrl = `www.ed-library.app/auth/callback`;
 
     await account.createOAuth2Session(
       OAuthProvider.Google,
