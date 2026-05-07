e482e3c97231ae0a5fab07c9c1e42461f79290bd
Dean Blessed <deanb6538@gmail.com>
2026-05-07 18:51:50 +0100

    Fixed

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['fra.cloud.appwrite.io', "ed-library-bucket.s3.us-east-1.amazonaws.com"], // your Appwrite domain
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fra.cloud.appwrite.io",
+      },
+      {
+        protocol: "https",
+        hostname: "ed-library-bucket.s3.us-east-1.amazonaws.com",
+      },
       {
         protocol: "https",
         hostname: "ui-avatars.com",
